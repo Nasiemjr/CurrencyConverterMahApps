57f0c6ad4c0f1303c56562941aaf5e95d05c56e5
Nasiemjr <Nasiem1610@gmail.com>
2023-10-07 22:56:16 +0200

    Added items source for combobox

---
+++ MainVM.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverterMahApps;
+
+public class MainVM : ObservableObject
+{
+	public MainVM()
+	{
+		Countries = new();
+		GetCountries();
+	}
+
+	/// <summary>
+	/// Returns the list of countries for the from option
+	/// </summary>
+	private void GetCountries()
+	{
+        Countries.Add("--SELECT--");
+        Countries.Add("INR");
+        Countries.Add("USD");
+        Countries.Add("EUR");
+        Countries.Add("SAR");
+        Countries.Add("POUND");
+        Countries.Add("DEM");
+    }
+
+	public List<String> Countries { get; }
+
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace CurrencyConverterMahApps
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainVM();
         }
     }
 }
