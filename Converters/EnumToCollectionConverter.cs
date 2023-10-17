f658a7509c8a5f6f66955a7684de7131906219e3
Nasiemjr <Nasiem1610@gmail.com>
2023-10-17 22:21:38 +0200

    Now using enums for the combo boxes.

---
+++ Converters/EnumToCollectionConverter.cs
@@ -0,0 +1,29 @@
+﻿using CurrencyConverterMahApps.Helper_Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CurrencyConverterMahApps.Helper_Classes.EnumHelper;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CurrencyConverterMahApps.Converters;
+
+[ValueConversion(typeof(Enum), typeof(IEnumerable<ValueDescription>))]
+public class EnumToCollectionConverter : MarkupExtension, IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return EnumHelper.GetAllValuesAndDescriptions(value.GetType());
+    }
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return this;
+    }
+}
--- MainVM.cs
+++ MainVM.cs
@@ -1,17 +1,37 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CurrencyConverterMahApps;
 
 public class MainVM : ObservableObject
 {
+	public enum COUNTRIES
+	{
+		[Description("Indian Rupee")]
+		INR,
+		[Description("United States Dollar")]
+        USD,
+        [Description("Euro")]
+        EUR,
+        [Description("Saudi Riyal")]
+        SAR,
+        [Description("Pound")]
+        POUND,
+        [Description("Deutchmark")]
+        DEM
+    }
+
 	public MainVM()
 	{
-		Countries = new();
+        _convertCmd = new RelayCommand(ConvertCurrency, CanConvert);
+        Countries = new();
 		GetCountries();
 	}
 
@@ -29,6 +49,78 @@ public class MainVM : ObservableObject
         Countries.Add("DEM");
     }
 
+
+    public void ConvertCurrency()
+    {
+        var selectedCount = SelectedCountry;
+    }
+
+    public bool CanConvert()
+    {
+        return true;
+    }
+
+
 	public List<String> Countries { get; }
+
+    public COUNTRIES SelectedCountry
+    { 
+        get => _selectedCountry;
+
+        set {
+                if (_selectedCountry != value)
+                {
+                    SetProperty(ref _selectedCountry, value);
+                }
+            }
+    }
+
+    public int SelectedCountryIndex
+    {
+        get => _selectedCountryIndex;
+
+        set
+        {
+            SetProperty(ref _selectedCountryIndex, value);
+        }
+    }
+
+    public COUNTRIES SelectedToCountry
+    {
+        get => _selectedToCountry;
+
+        set
+        {
+            if (_selectedToCountry != value)
+            {
+                SetProperty(ref _selectedToCountry, value);
+            }
+        }
+    }
+
+    public int SelectedToCountryIndex
+    {
+        get => _selectedToCountryIndex;
+
+        set
+        {
+            SetProperty(ref _selectedToCountryIndex, value);
+        }
+    }
+
+    public ICommand ConvertCommand
+    {
+        get => _convertCmd;
+        set
+        {
+            SetProperty(ref _convertCmd, value);
+        }
+    }
+
+    private ICommand _convertCmd;
+    private COUNTRIES _selectedCountry;
+    private int _selectedCountryIndex = -1; 
+    private COUNTRIES _selectedToCountry;
+    private int _selectedToCountryIndex = -1;
 
 }
